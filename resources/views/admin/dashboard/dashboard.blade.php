b750fd3b3f490432a3715187b3a9234d47e291b4
Prashant Bhore <prashant.b@mplussoft.in>
2023-11-17 19:11:06 +0530

    intiate

---
+++ resources/views/admin/dashboard/dashboard.blade.php
@@ -0,0 +1,31 @@
+@extends('admin.layout.layout')
+
+@section("content")
+
+<section class="main-content bg-info">
+    <h1 class="display-1 text-white">Hello there!</h1>
+    <h1 class="display-1 text-white">Hello there!</h1>
+    <h1 class="display-1 text-white">Hello there!</h1>
+    <h1 class="display-1 text-white">Hello there!</h1>
+    <h1 class="display-1 text-white">Hello there!</h1>
+    <h1 class="display-1 text-white">Hello there!</h1>
+    <h1 class="display-1 text-white">Hello there!</h1>
+    <h1 class="display-1 text-white">Hello there!</h1>
+    <h1 class="display-1 text-white">Hello there!</h1>
+    <h1 class="display-1 text-white">Hello there!</h1>
+    <h1 class="display-1 text-white">Hello there!</h1>
+    <h1 class="display-1 text-white">Hello there!</h1>
+    <h1 class="display-1 text-white">Hello there!</h1>
+    <h1 class="display-1 text-white">Hello there!</h1>
+    <h1 class="display-1 text-white">Hello there!</h1>
+    <h1 class="display-1 text-white">Hello there!</h1>
+    <h1 class="display-1 text-white">Hello there!</h1>
+    <h1 class="display-1 text-white">Hello there!</h1>
+    <h1 class="display-1 text-white">Hello there!</h1>
+    <h1 class="display-1 text-white">Hello there!</h1>
+    <h1 class="display-1 text-white">Hello there!</h1>
+</section>
+@endsection
+
+@section('script')
+@endsection
---
+++ resources/views/admin/layout/layout.blade.php
@@ -0,0 +1,17 @@
+@include('admin.includes.head-files')
+@include('admin.includes.navbar')
+
+<main class="sidebar-menu">
+    @include('admin.includes.sidebar')
+</main>
+@yield('content')
+
+
+<!-- @include('admin.includes.footer') -->
+@include('admin.includes.js-files')
+
+@yield('script')
+
+</body>
+
+</html>
