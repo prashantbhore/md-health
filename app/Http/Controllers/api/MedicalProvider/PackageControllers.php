9949ff90968a07e77a0897e73d4e8135c9bdefc1
Ajay Prajapati <ajay.p@mplussoft.in>
2023-12-01 10:38:27 +0530

    changes in development

---
+++ app/Http/Controllers/api/MedicalProvider/PackageControllers.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\api\MedicalProvider;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Controllers\api\BaseController as BaseController;
+use Validator;
+use App\Traits\MediaTrait;
+use Str;
+use Auth;
+use App\Models\AddNewAcommodition;
+use App\Models\Packages;
+use App\Models\ToursDetails;
+use App\Models\TransportationDetails;
+
+
+class PackageControllers extends BaseController
+{
+    //
+}
