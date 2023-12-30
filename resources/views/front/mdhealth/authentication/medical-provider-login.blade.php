51e91fe769909e002819db9ed202aedc02078113
pratikpatil <pratikpatil7110@gmail.com>
2023-12-30 12:29:48 +0530

    chnages

--- resources/views/front/mdhealth/authentication/medical-provider-login.blade.php
+++ resources/views/front/mdhealth/authentication/medical-provider-login.blade.php
@@ -730,7 +730,7 @@
     </script>
 
     <script>
-        $(".toggle-password").click(function() {
+        $(".toggle-password").click(function(){
         $(this).toggleClass("fa-eye fa-eye-slash");
         var input = $($(this).attr("toggle"));
         if (input.attr("type") == "password") {
