35198cf4c98e800e230ddd188c08aefe2bfa1c92
Prashant Bhore <prashant.b@mplussoft.in>
2024-01-05 16:08:51 +0530

    design changes

--- resources/views/front/mdhealth/medical-provider/dashboard.blade.php
+++ resources/views/front/mdhealth/medical-provider/dashboard.blade.php
@@ -25,44 +25,33 @@
                 </div>
 
                 <!-- RECENT TRETMENTS -->
-                <div class="card">
+                <div class="card" style="min-height: 218px;">
                     <h5 class="card-header d-flex align-items-center justify-content-between mb-3">
                         Recent Treatment Orders
                     </h5>
                     <div class="card-body">
-                         @if($recent_orders)
-                         @foreach ($recent_orders as $order)
+                        @if($recent_orders)
+                        @foreach ($recent_orders as $order)
 
-                           {{-- {{dd($order)}} --}}
-                             
-                       
-                        <div class="treatment-card df-start w-100 mb-3">
-                            <div class="row card-row align-items-center">
-                                <div class="col-md-2 df-center px-0">
+                        {{-- {{dd($order)}} --}}
+                        <!--  -->
+                        <div class="card shadow-none" style="border-radius: 3px;background: #F6F6F6;">
+                            <div class="p-3 d-flex gap-3">
+                                <div>
                                     <img src="{{!empty($provider_logo->company_logo_image_path)?url('/').Storage::url($provider_logo->company_logo_image_path):asset('front/assets/img/Memorial.svg')}}" alt="">
                                 </div>
-                                <div class="col-md-6 justify-content-start ps-0">
-                                    <div class="trmt-card-body">
-                                        <h5 class="dashboard-card-title">Treatment No: #{{!empty($order['order_id'])?$order['order_id']:''}}<span class="pending">{{!empty($order['purchase_type'])?$order['purchase_type']:''}}</span></h5>
-                                        <h5 class="mb-0 fw-500">{{!empty($order['package']['package_name'])?$order['package']['package_name']:''}}</h5>
-                                    </div>
+                                <div>
+                                    <h5 class="card-h1">Treatment No: #{{!empty($order['order_id'])?$order['order_id']:''}} <span class="pending ms-3">{{!empty($order['purchase_type'])?$order['purchase_type']:''}}</span></h5>
+                                    <p class="mb-0 pkg-name">{{!empty($order['package']['package_name'])?$order['package']['package_name']:''}}</p>
                                 </div>
-                                <div class="col-md-4 d-flex flex-column justify-content-between align-items-end text-end">
-                                    <div class="trmt-card-footer">
-                                        <h6 class="dbrd-order-total"><strong>Total Price:</strong> <span class="">{{!empty($order['package']['package_price'])?$order['package']['package_price']:''}} ₺</span></h6>
-                                        {{-- <h6 class="fw-700">Order ID: #MD3726378</h6> --}}
-                                        {{-- <a href="{{url('treatment-order-details')}}" class="mt-auto view-detail-btn"><strong>View Details</strong></a> --}}
-
-                                        <a href="{{ url('treatment-order-details/' . (!empty($order['id']) ?Crypt::encrypt($order['id']): '')) }}" class="mt-auto view-detail-btn"><strong>View Details</strong></a>
-                                    </div>
+                                <div class="ms-auto d-flex flex-column justify-content-end align-items-end">
+                                    <h5 class="card-h3 mb-0">Total Price: <span class="card-p1">{{!empty($order['package']['package_price'])?$order['package']['package_price']:''}} ₺</span></h5>
+                                    <a href="{{ url('treatment-order-details/' . (!empty($order['id']) ?Crypt::encrypt($order['id']): '')) }}" class="mt-auto view-det">View Details</a>
                                 </div>
                             </div>
                         </div>
-
                         @endforeach
                         @endif
-
-
                     </div>
                 </div>
             </div>
