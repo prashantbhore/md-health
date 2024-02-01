c41055ac9a778a7e1e0729c3f9b1cbfb3c9ee0a8
mohitmkg65 <mohitmkg65@gmail.com>
2024-02-01 16:42:36 +0530

    Mplus02

--- app/Http/Controllers/GetCityController.php
+++ app/Http/Controllers/GetCityController.php
@@ -2,15 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Cities;
 
-class GetCityController extends Controller {
-    public function get_cities_of_country( $country_id ) {
-        $cities = Cities::where( 'country_id', $country_id )->get();
+class GetCityController extends Controller
+{
+    public function get_cities_of_country($country_id)
+    {
+        // dd($country_id);
+        $cities = Cities::where('country_id', $country_id)->get();
+        if ($cities->isEmpty()) {
+            return response()->json([
+                'message' => 'no cities found',
+            ], 404);
+        }
+        $new_city[] = [];
+        foreach ($cities as $key => $city) {
+            $new_city[$key]['id'] = $city->id;
+            $new_city[$key]['city_name'] = $city->city_name;
+        }
+        $cities = $new_city;
+        // dd($cities);
         // Assuming you have a blade partial for cities dropdown options
-        return request()->json( [
-            'cities' => $cities
-        ] );
+        return response()->json([
+            'message' => 'cities found',
+            'cities' => $cities,
+        ]);
     }
 }
